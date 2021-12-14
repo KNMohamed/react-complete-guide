da4b81878288fc74c5c60304edeaa7a424dfa9bb
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2021-12-14 11:34:58 -0500

    Render expense list and add filter functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,35 @@
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import ExpenseFilter from '../ExpenseFilter/ExpenseFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2021');
+  const selectedDateChangeHandler = (selectedDate) => {
+    setFilteredYear(selectedDate);
+  };
+
   return (
-    <div className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+    <div>
+      <Card className='expenses'>
+        <ExpenseFilter
+          selected={filteredYear}
+          onSelectedDateChange={selectedDateChangeHandler}
+        />
+        {props.items
+          .filter((item) => item.date.getFullYear() === parseInt(filteredYear))
+          .map((item) => (
+            <ExpenseItem
+              key={item.id}
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
+          ))}
+      </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
